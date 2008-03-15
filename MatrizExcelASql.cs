d26b2ba34dbc8220bfd719a82343953eea3883d7
emilioplatzer <emilioplatzer@16dbb305-c246-0410-9252-eb3b77155df1>
2008-03-15 17:46:52 +0000

    trabajando. Sin verificar casos

---
+++ MatrizExcelASql.cs
@@ -0,0 +1,27 @@
+/*
+ * Created by SharpDevelop.
+ * User: Administrador
+ * Date: 15/03/2008
+ * Time: 01:51 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+
+namespace TodoASql
+{
+	/// <summary>
+	/// Description of MatrizExcelASql.
+	/// </summary>
+	public class MatrizExcelASql
+	{
+		ReceptorSql Receptor;
+		public MatrizExcelASql(ReceptorSql receptor){
+			this.Receptor=receptor;
+		}
+		public PasarHoja(RangoExcel matriz,RangoExcel[] filas, RangoExcel[] columnas){
+			HojaExcel Hoja=hoja;
+		}
+	}
+}
