f3d8a8a26cdbc270c11d481b89d958304026c7ac
emilioplatzer <emilioplatzer@16dbb305-c246-0410-9252-eb3b77155df1>
2008-03-15 13:57:28 +0000

    Pruebas con Excel

--- Excel.cs
+++ Excel.cs
@@ -18,50 +18,75 @@ namespace TodoASql
 	/// </summary>
 	public class UnExcel
 	{
-		private static Excel.Application ApExcel;
+		private static Excel.Application apExcel;
+		public static Excel.Application ApExcel{
+			get{
+				if(apExcel==null) apExcel = new Excel.Application();
+				return apExcel;
+			}
+		}
 		private UnExcel(){
 			
 		}
 		static UnExcel()
 		{
 			AppDomain.CurrentDomain.DomainUnload+= delegate { 
-				ApExcel.Quit();
-				ApExcel=null;
+				if(apExcel!=null){
+					apExcel.Quit();
+					apExcel=null;
+				}
 			};
-		}
-		public static Excel.Workbook Nuevo(string nombreArchivo){
-			if(ApExcel==null) ApExcel = new Excel.Application();
-			Archivo.Borrar(nombreArchivo);
-			Assert.IsFalse(Archivo.Existe(nombreArchivo));
-			object opc = Type.Missing;
-			Excel.Workbook libro;
-			libro = ApExcel.Workbooks.Add(opc);
-			ApExcel.Visible = true;
-			libro = ApExcel.Workbooks.Add(opc);
-			Excel.Worksheet hoja1 = new Excel.Worksheet();
-			hoja1 = (Excel.Worksheet)libro.Sheets.Add(opc, opc, opc, opc);
-			hoja1.Activate();
-			hoja1.Name="LaHoja1";
-			libro.SaveAs(nombreArchivo,opc,opc,opc,opc,opc,
-			             Excel.XlSaveAsAccessMode.xlNoChange,
-			             Excel.XlSaveConflictResolution.xlLocalSessionChanges,
-			             opc,opc,opc,opc);
-			libro.Close(opc,opc,opc);
-			return libro;
 		}
 	}
 	[TestFixture]
-	public class probarElExcel{
+	public class probarExcel{
+		Excel.Application ApExcel;
+		object ___ = Type.Missing; 
+		string nombreArchivo=Archivo.CarpetaActual()+"\\borrar_prueba.xls";
+		public probarExcel(){
+			ApExcel = new Excel.Application();
+		}
+		~probarExcel(){
+			ApExcel.Quit();
+		}
 		[Test]
 		public void CrearArchivo(){
-			string archivo=Archivo.CarpetaActual()+"\\borrar_prueba.xls";
-			Excel.Workbook libro=UnExcel.CrearArchivo(archivo);
-			Assert.IsTrue(Archivo.Existe(archivo));
+			Archivo.Borrar(nombreArchivo);
+			Assert.IsFalse(Archivo.Existe(nombreArchivo));
+			Excel.Workbook libro;
+			libro = ApExcel.Workbooks.Add(___);
+			ApExcel.Visible = true;
+			libro = ApExcel.Workbooks.Add(___);
+			Excel.Worksheet hoja1 = new Excel.Worksheet();
+			hoja1 = (Excel.Worksheet)libro.Sheets.Add(___, ___, ___, ___);
+			hoja1.Activate();
+			hoja1.Name="LaHoja1";
+			libro.SaveAs(nombreArchivo,___,___,___,___,___,
+			             Excel.XlSaveAsAccessMode.xlNoChange,
+			             Excel.XlSaveConflictResolution.xlLocalSessionChanges,
+			             ___,___,___,___);
+			libro.Close(___,___,___);
+			Assert.IsTrue(Archivo.Existe(nombreArchivo));
+		}
+		[Test]
+		public void MeterAlgunosDatos(){
+			Excel.Workbook libro=ApExcel.Workbooks.Open(nombreArchivo,___,___,___,___,___,___,___,___,___,___,___,___,___,___);
 			Excel.Worksheet hoja=(Excel.Worksheet) libro.Worksheets["LaHoja1"];
-			// Excel.Worksheet hoja=libro.Worksheets.Item("LaHoja1");  // ["LaHoja1"];
 			hoja.Cells[1,1]="uno";
 			hoja.Cells[2,2]="dos";
+			hoja.Cells[3,14]="pi";
 			libro.Save();
+		}
+		[Test]
+		public void LeerAlgunosDatos(){
+			Excel.Workbook libro=ApExcel.Workbooks.Open(nombreArchivo,___,___,___,___,___,___,___,___,___,___,___,___,___,___);
+			Excel.Worksheet hoja=(Excel.Worksheet) libro.Worksheets["LaHoja1"];
+			// Excel.Range r=hoja.Range["A1"];
+			Excel.Range r;
+			r=hoja.get_Range("A1",___);
+			Assert.AreEqual("uno",r.Text.ToString());
+			Assert.AreEqual("dos",((Excel.Range) hoja.Cells[2,2]).Text);
+			Assert.AreEqual("pi",hoja.get_Range("N3",___).Text);
 		}
 	}
 }
