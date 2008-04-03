d17d88d9383f467d1a7576b8e05968d3ca74d16d
emilioplatzer <emilioplatzer@16dbb305-c246-0410-9252-eb3b77155df1>
2008-04-03 14:14:15 +0000

    Había un error en la barra invertida al grabarla a postgre

--- trunk/soporte/PostgreCrearObjetosEnLaBase.sql
+++ trunk/soporte/PostgreCrearObjetosEnLaBase.sql
@@ -1,13 +1,14 @@
 CREATE ROLE import2sql LOGIN
   ENCRYPTED PASSWORD 'md5ed22fdc74436e11b55e3b96292a4df8b'
   NOSUPERUSER NOINHERIT NOCREATEDB NOCREATEROLE;
-CREATE DATABASE import2sqlDB
+CREATE DATABASE "import2sqlDB"
   WITH OWNER = import2sql
        ENCODING = 'LATIN1'
        TABLESPACE = pg_default;
-CREATE TABLE "TablaExistente"
+CREATE TABLE TablaExistente
 (
    texto character varying(100),
    numero integer
 ) WITHOUT OIDS;
-ALTER TABLE "TablaExistente" OWNER TO import2sql;
+ALTER TABLE TablaExistente OWNER TO import2sql;
+INSERT INTO tablaexistente (texto, numero) VALUES ('uno',1);
