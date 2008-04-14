5e3184daaa292c55f482923a9816df8ab7e1787e
emilioplatzer <emilioplatzer@16dbb305-c246-0410-9252-eb3b77155df1>
2008-04-14 14:06:51 +0000

    Modelador con Fk

--- trunk/Modelador/PrModelador.cs
+++ trunk/Modelador/PrModelador.cs
@@ -59,7 +59,7 @@ namespace PrModelador
 		public void SentenciaInsert(){
 			Productos p=new Productos();
 			BaseDatos dba=BdAccess.SinAbrir();
-			Assert.AreEqual("INSERT INTO [productos] ([producto], [nombreproducto]) SELECT p.[producto], p.[producto] AS [nombreproducto]\n FROM [productos] p;",
+			Assert.AreEqual("INSERT INTO [productos] ([producto], [nombreproducto]) SELECT p.[producto], p.[producto] AS [nombreproducto]\n FROM [productos] p;\n",
 				new Ejecutador(dba)
 				.Dump(new SentenciaInsert(new Productos()).Select(p.cProducto,p.cNombreProducto.Es(p.cProducto))));
 		}
@@ -67,11 +67,11 @@ namespace PrModelador
 		public void SentenciaUpdate(){
 			Productos p=new Productos();
 			BaseDatos dba=BdAccess.SinAbrir();
-			Assert.AreEqual("UPDATE [productos] SET [producto]='P1', [nombreproducto]='Producto 1';",
+			Assert.AreEqual("UPDATE [productos] SET [producto]='P1', [nombreproducto]='Producto 1';\n",
 			                new Ejecutador(dba)
 			                .Dump(new SentenciaUpdate(p,p.cProducto.Set("P1"),p.cNombreProducto.Set("Producto 1"))));
 			string Esperado="UPDATE [productos] SET [producto]='P1', [nombreproducto]='Producto 1'\n WHERE [producto]='P3'\n AND ([nombreproducto] IS NULL OR [nombreproducto]<>[producto])";
-			Assert.AreEqual(Esperado+";",
+			Assert.AreEqual(Esperado+";\n",
 			                new Ejecutador(dba)
 			                .Dump(new SentenciaUpdate(p,p.cProducto.Set("P1"),p.cNombreProducto.Set("Producto 1"))
 			                      .Where(p.cProducto.Igual("P3")
@@ -85,13 +85,13 @@ namespace PrModelador
 			Assert.AreEqual("productos",sentencia.Tablas()[0].NombreTabla);
 			sentencia.Where(p.cNombreProducto.Distinto("P0"));
 			Esperado+="\n AND [nombreproducto]<>'P0'";
-			Assert.AreEqual(Esperado+";",new Ejecutador(dba).Dump(sentencia));
-			Assert.AreEqual(Esperado+";",new Ejecutador(dba).Dump(sentencia));
+			Assert.AreEqual(Esperado+";\n",new Ejecutador(dba).Dump(sentencia));
+			Assert.AreEqual(Esperado+";\n",new Ejecutador(dba).Dump(sentencia));
 			Empresas contexto=new Empresas();
 			contexto.cEmpresa.AsignarValor(14);
 			using(Ejecutador ej=new Ejecutador(dba,contexto)){
 				Esperado+="\n AND [empresa]=14";
-				Assert.AreEqual(Esperado+";",ej.Dump(sentencia));
+				Assert.AreEqual(Esperado+";\n",ej.Dump(sentencia));
 			}
 		}	
 		[Test]
@@ -104,15 +104,15 @@ namespace PrModelador
 			using(Ejecutador ej=new Ejecutador(dba,e)){
 				Sentencia s=
 					new SentenciaSelect(e.cEmpresa,e.cNombreEmpresa);
-				Assert.AreEqual("SELECT e.[empresa], e.[nombreempresa]\n FROM [empresas] e\n WHERE e.[empresa]=13;",
+				Assert.AreEqual("SELECT e.[empresa], e.[nombreempresa]\n FROM [empresas] e\n WHERE e.[empresa]=13;\n",
 				                ej.Dump(s));
-				PartesProductos pp=new PartesProductos();
-				/*
+				PartesProductos pp=new PartesProductos(); 
+				pp.UsarFk();
+				Productos pr=pp.fkProductos;
 				Sentencia s2=
-					new SentenciaSelect(p.cProducto,p.cNombreProducto,cCantidadPartes.EsSuma(pp.cCantidad));
-				Assert.AreEqual("SELECT p.[producto], p.[nombreproducto], SUM(pa.[cantidad]) AS [cantidadpartes]\n FROM [productos] p, [partesproductos] pa\n WHERE p.[empresa]=13\n AND p.[empresa]=pa.[empresa]\n AND p.[producto]=pa.[producto];\n"
+					new SentenciaSelect(pr.cProducto,pr.cNombreProducto,cCantidadPartes.EsSuma(pp.cCantidad));
+				Assert.AreEqual("SELECT p.[producto], p.[nombreproducto], SUM(pa.[cantidad]) AS [cantidadpartes]\n FROM [productos] p, [partesproductos] pa\n WHERE p.[empresa]=pa.[empresa]\n AND p.[producto]=pa.[producto]\n AND p.[empresa]=13\n AND pa.[empresa]=13;\n"
 				                ,ej.Dump(s2));
-				                */
 			}
 		}
 	}
