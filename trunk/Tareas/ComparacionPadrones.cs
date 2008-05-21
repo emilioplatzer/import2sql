8b4619db70687bf81f0f415e185c660c47872a4f
emilioplatzer <emilioplatzer@16dbb305-c246-0410-9252-eb3b77155df1>
2008-05-21 19:09:45 +0000

    npp. Haciendo el matching reordenando palabras

--- trunk/Tareas/ComparacionPadrones.cs
+++ trunk/Tareas/ComparacionPadrones.cs
@@ -58,7 +58,7 @@ namespace Tareas
 			:base(db)
 		{
 		}
-		public void AsignacionExacta(){
+		public void AsignacionOrdenada(){
 			Asignacion a=new Asignacion();
 			Nombres n=new Nombres();
 			n.Alias="n";
@@ -68,17 +68,95 @@ namespace Tareas
 				ej.Ejecutar(
 					new SentenciaInsert(a)
 						.Select(a.cNombreViejo.Es(v.cNombre),a.cNombreNuevo.Es(n.cNombre),a.cMetodo.Es("exacta"))
-						.Where(v.cNombre.Igual(n.cNombre).And(v.cConjunto.Igual("viejo")).And(n.cConjunto.Igual("nuevo")))
+						.Where(v.cNombreReordenado.Igual(n.cNombreReordenado).And(v.cConjunto.Igual("viejo")).And(n.cConjunto.Igual("nuevo")))
 				);
 			}
 		}
-		public void SepararPalabras(){
-			/*
+		public void AsignacionExacta(){
+			Asignacion a=new Asignacion();
 			Nombres n=new Nombres();
-			Palabras p=new NombresPalabras();
-			Numeros num=new Numeros();
-			*/
-			
+			n.Alias="n";
+			using(Ejecutador ej=new Ejecutador(db)){
+				ej.Ejecutar(
+					new SentenciaUpdate(n,n.cNombreReordenado.Es(n.cNombre))
+				);
+			}
+			AsignacionOrdenada();
+		}
+		public void SepararPalabras(){
+			Nombres nombres=new ComparacionPadrones.Nombres();
+			NombresPalabras nomprespalabras=new NombresPalabras();
+			ListaCampos pkEnAmbos=nombres.CamposPk();
+			pkEnAmbos.RemoveAll(campo => !nomprespalabras.ContieneMismoNombre(campo));
+			db.ExecuteNonQuery(@"CREATE OR REPLACE FUNCTION SEPARARPALABRAS() RETURNS VOID AS
+$$
+declare
+	pos integer;
+	v_palabra character varying (200);
+	v_frase character varying (200);
+	c record;
+	i integer;
+begin
+	for c in select {PKS} from {TABLA} GROUP BY {PKS} loop
+		v_frase:=c.nombre;
+		v_palabra:='';
+		loop
+			i:=1;
+			v_palabra=primerpalabra(v_frase);
+			v_frase=sinprimerpalabra(v_frase);
+			begin
+				insert into {TABLA}palabras ({PKS},palabra,orden) values ({C.PKS},v_palabra,i);
+				i:=i+1;
+			exception 
+				when unique_violation then
+				    null;
+			end;
+		exit when v_frase is null or v_frase='';
+		end loop;
+	end loop;
+end;
+$$
+LANGUAGE plpgsql".Replace("{TABLA}",nombres.NombreTabla)
+	               .Replace("{PKS}",Separador.Concatenar(pkEnAmbos.ConvertAll(campo => campo.NombreCampo),","))
+	               .Replace("{C.PKS}",Separador.Concatenar(pkEnAmbos.ConvertAll(campo => "c."+campo.NombreCampo),","))
+	);
+			db.ExecuteNonQuery("SELECT SEPARARPALABRAS();");
+		}
+		public void ReordenarPalabras(){
+			Nombres nombres=new ComparacionPadrones.Nombres();
+			NombresPalabras nomprespalabras=new NombresPalabras();
+			ListaCampos pkEnAmbos=nombres.CamposPk();
+			pkEnAmbos.RemoveAll(campo => !nomprespalabras.ContieneMismoNombre(campo));
+			db.ExecuteNonQuery(@"CREATE OR REPLACE FUNCTION REORDENARPALABRAS() RETURNS VOID AS
+$$
+begin
+	declare
+		c_frase record;
+		c_palabra record;
+		ordenada text;
+	begin
+		for c_frase in select * from {TABLA} where nombrereordenado is null loop
+			ordenada:='';
+			for c_palabra in 
+				select * 
+					from {TABLA}palabras
+					where {JOIN}
+					order by palabra 
+			loop
+				ordenada:=ordenada||c_palabra.palabra||' ';
+			end loop;
+			begin
+				update {TABLA}
+					set nombreReordenado=ordenada
+					where {JOIN};
+			end;
+		end loop;
+	end;
+end;
+$$
+LANGUAGE plpgsql".Replace("{TABLA}",nombres.NombreTabla)
+			                   .Replace("{JOIN}",Separador.Concatenar(pkEnAmbos.ConvertAll(campo => campo.NombreCampo+"=c_frase."+campo.NombreCampo)," AND ")));
+			db.ExecuteNonQuery("SELECT REORDENARPALABRAS();");
 		}
 	}
 	[TestFixture]
@@ -87,7 +165,7 @@ namespace Tareas
 		ComparacionPadrones cp;
 		public PrComparacionPadrones(){
 			#pragma warning disable 162
-			switch(3){ // Solo se va a tomar un camino
+			switch(1){ // Solo se va a tomar un camino
 				case 1:
 					db=PostgreSql.Abrir("127.0.0.1","import2sqlDB","import2sql","sqlimport");
 					cp=new ComparacionPadrones(db);
@@ -104,6 +182,12 @@ namespace Tareas
 					break;
 			}
 			#pragma warning restore 162
+		}
+		public void Vaciar(){
+			Ejecutador ej=new Ejecutador(db);
+			ej.Ejecutar(new SentenciaDelete(new ComparacionPadrones.NombresPalabras()));
+			ej.Ejecutar(new SentenciaDelete(new ComparacionPadrones.Asignacion()));
+			ej.Ejecutar(new SentenciaDelete(new ComparacionPadrones.Nombres()));
 		}
 		public void Cargar1(string[] datos,string conjunto){
 			ComparacionPadrones.Nombres n=new ComparacionPadrones.Nombres();
@@ -135,10 +219,10 @@ namespace Tareas
 			Cargar(viejos,nuevos);
 			cp.AsignacionExacta();
 			CompararCoincidencias(coinciden,coinciden);
-			cp.SepararPalabras();
 		}
 		[Test]
 		public void Pr02ExtraccionPalabras(){
+			cp.SepararPalabras();
 			var n=new ComparacionPadrones.Nombres();
 			if(db is BdAccess){
 				Assert.Ignore("Todavía no se puede sacar la primera palabra en Access");
@@ -157,16 +241,33 @@ namespace Tareas
 				dr.Read();
 				Assert.AreEqual(" distinta ",dr["nombre"]);
 				Assert.AreEqual("distinta",dr["primerapalabra"]);
-				Assert.Ignore("Revisar con distintas configuraciones, a veces no anda lo que sigue");
-				Assert.IsNull(dr["sinprimerapalabra"]);
+				// Assert.Ignore("Revisar con distintas configuraciones, a veces no anda lo que sigue");
+				Assert.AreEqual("",dr["sinprimerapalabra"]);
 				dr.Read();
 				Assert.AreEqual("Belgrano",dr["primerapalabra"]);
-				Assert.IsNull(dr["sinprimerapalabra"]);
+				Assert.AreEqual("",dr["sinprimerapalabra"]);
 				dr.Read();
 				dr.Read();
 				Assert.AreEqual("No",dr["primerapalabra"]);
 				Assert.AreEqual("esta",dr["sinprimerapalabra"]);
 			}
+		}
+		[Test]
+		public void Pr03Reordenacion(){
+			cp.ReordenarPalabras();
+			string[] viejos={"San Martín","Belgrano","Corrientes","No esta"," distinta "};
+			string[] nuevos={"Martín San","Belgrano","Corrientes","Tampoco esta","distinta"};
+			string[] coinciden1={"Belgrano","Corrientes"};
+			string[] coinciden2v={"Belgrano","Corrientes","San Martín"};
+			string[] coinciden2n={"Belgrano","Corrientes","Martín San"};
+			Vaciar();
+			Cargar(viejos,nuevos);
+			cp.AsignacionExacta();
+			CompararCoincidencias(coinciden1,coinciden1);
+			cp.SepararPalabras();
+			cp.ReordenarPalabras();
+			cp.AsignacionOrdenada();
+			CompararCoincidencias(coinciden2v,coinciden2n);			
 		}
 	}
 }
